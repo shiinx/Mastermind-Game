2772f4807179fd1e90ee534092c3774b772aef01
shiinx <z.shiinx@gmail.com>
2015-01-24 00:02:27 +0800

    to revert

---
+++ Coding stuff all here/Mastermind Game/ScoreBoard.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace Mastermind_Game
+{
+    public partial class ScoreBoard : Form
+    {
+        public ScoreBoard()
+        {
+            InitializeComponent();
+        }
+
+        private void ScoreBoard_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
